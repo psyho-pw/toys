4868d744c3877c07c695a567db2d89b04c0284a2
sayho <fishcreek@naver.com>
2023-04-29 23:53:23 +0900

    fix: misconfig

--- apps/toys/src/app.module.ts
+++ apps/toys/src/app.module.ts
@@ -1,10 +1,20 @@
 import {MiddlewareConsumer, Module, NestModule} from '@nestjs/common'
+import * as Joi from 'joi'
 import {AppController} from './app.controller'
 import {AppService} from './app.service'
 import {LoggerMiddleware} from '@app/common/middlewares/logger.middleware'
+import {ConfigModule} from '@nestjs/config'
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: ['./apps/toys/.env'],
+            validationSchema: Joi.object({
+                PORT: Joi.number().required(),
+            }),
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
--- apps/toys/src/main.ts
+++ apps/toys/src/main.ts
@@ -1,7 +1,6 @@
 import {NestFactory} from '@nestjs/core'
 import {AppModule} from './app.module'
 import {Logger} from '@nestjs/common'
-import process from 'process'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
